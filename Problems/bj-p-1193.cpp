ff8bdb33bc1e444bdfb69498f8c149da555fe096
Xerlocked <sherlock203b@gmail.com>
2025-09-03 15:09:09 +0900

    2025.09.03 bj 5 problems

---
+++ Problems/bj-p-1193.cpp
@@ -0,0 +1,40 @@
+// https://www.acmicpc.net/problem/1193
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long ll;typedef long double ld;typedef pair<int,int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+void solve()
+{
+    int x;
+    cin >> x;
+
+    int g = 1;
+    while (x > g)
+    {
+        x -= g;
+        g++;
+    }
+
+    if (g % 2 == 0)
+    {
+        cout << x << "/" << (g+1) - x;
+    }
+    else
+    {
+        cout << (g+1) - x << "/" << x;
+    }
+}
+
+signed main()
+{
+    fast;
+    solve();
+    return 0;
+}
