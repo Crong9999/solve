188f376ea76b48c452dbee2a11a06724e9ae3e93
Xerlocked <sherlock203b@gmail.com>
2025-09-06 02:12:41 +0900

    2025.09.06 bj 2 problems

---
+++ Problems/bj-p-1013.cpp
@@ -0,0 +1,44 @@
+// https://www.acmicpc.net/problem/1013
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long ll;typedef long double ld;typedef pair<int,int> pii;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
+ll powe(ll x, ll y){ x = x%mod, y=y%(mod-1);ll ans = 1;while(y>0){if (y&1){ans = (1ll * x * ans)%mod;}y>>=1;x = (1ll * x * x)%mod;}return ans;}
+
+void solve()
+{
+
+    int t;
+    cin >> t;
+
+    while (t > 0)
+    {
+
+        string s;
+        cin >> s;
+        
+        regex pattern("^(100+1+|01)+$");
+        
+        if (regex_match(s, pattern))
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+        t--;
+    }
+}
+
+signed main()
+{
+    fast;
+    solve();
+    return 0;
+}
